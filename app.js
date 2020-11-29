dcfabd535048dc1cee24f6f866314a2b3557054f
bilkn <65674342+bilkn@users.noreply.github.com>
2020-11-29 18:39:43 +0300

    added new sections

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+const btn = document.querySelector(".menu-bar");
+let isMenuOpen = false;
+
+/* Event listeners */
+btn.addEventListener("click", ()=> {
+    if (!isMenuOpen) {
+        btn.classList.add("menu-bar--open");
+        isMenuOpen = true;
+    }
+    else {
+        btn.classList.remove("menu-bar--open")
+        isMenuOpen = false;
+    }  
+});
 

1f7c0ad2b2e5c3069fe4c0af7522159856d5573b
bilkn <65674342+bilkn@users.noreply.github.com>
2020-11-30 21:44:09 +0300

    added social links

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ let isMenuOpen = false;
 btn.addEventListener('click', () => {
   if (!isMenuOpen) {
     openMobileMenu();
+    addNoScroll(document.body);
   } else {
     closeMobileMenu();
+    removeNoScroll(document.body);
   }
 });
 
 mobileMenu.addEventListener('click', (e) => {
-  const menuItem = e.target.closest('.mobile-menu-item');
+  const menuItem = e.target.closest('.mobile-menu-nav-item');
   if (menuItem) {
     closeMobileMenu();
+    removeNoScroll(document.body);
   }
 });
 
@@ -29,4 +32,12 @@ function closeMobileMenu() {
   btn.classList.remove('menu-bar--open');
   mobileMenu.classList.remove('mobile-menu--open');
   isMenuOpen = false;
+}
+
+function addNoScroll (selector) {
+  selector.classList.add("no-scroll");
+}
+
+function removeNoScroll (selector) {
+  selector.classList.remove("no-scroll");
 }

34809ac89ba0039afd0b4aace8f573efa416456b
bilkn <65674342+bilkn@users.noreply.github.com>
2020-11-30 16:10:13 +0300

    added menu for mobile

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-const btn = document.querySelector(".menu-bar");
+const btn = document.querySelector('.menu-bar');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 let isMenuOpen = false;
 
 /* Event listeners */
-btn.addEventListener("click", ()=> {
-    if (!isMenuOpen) {
-        btn.classList.add("menu-bar--open");
-        isMenuOpen = true;
-    }
-    else {
-        btn.classList.remove("menu-bar--open")
-        isMenuOpen = false;
-    }  
+btn.addEventListener('click', () => {
+  if (!isMenuOpen) {
+    openMobileMenu();
+  } else {
+    closeMobileMenu();
+  }
 });
 
+mobileMenu.addEventListener('click', (e) => {
+  const menuItem = e.target.closest('.mobile-menu-item');
+  if (menuItem) {
+    closeMobileMenu();
+  }
+});
+
+function openMobileMenu() {
+  btn.classList.add('menu-bar--open');
+  mobileMenu.classList.add('mobile-menu--open');
+  isMenuOpen = true;
+}
+
+function closeMobileMenu() {
+  btn.classList.remove('menu-bar--open');
+  mobileMenu.classList.remove('mobile-menu--open');
+  isMenuOpen = false;
+}
